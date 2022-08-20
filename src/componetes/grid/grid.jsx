76a8622bfbbb51f5b36cc22aab3b5ad8896701e6
patriky1 <patrikybrito@gmail.com>
2022-08-20 14:54:59 -0300

    up

--- src/componetes/grid/grid.jsx
+++ src/componetes/grid/grid.jsx
@@ -14,7 +14,7 @@ function grid() {
   return (
     <>
       <div className="grid">
-        <video className="videobg" src={videovg} autoPlay muted loop />
+        {/* <video className="videobg" src={videovg} autoPlay muted loop /> */}
         <div className="content">
           <Col lg="1">
             <a className=" content1" href="#education">
