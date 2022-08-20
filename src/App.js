c2822cad6857f2ae6011960674e83856eb6a25a2
patriky1 <patrikybrito@gmail.com>
2022-08-20 14:43:26 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "./App.css";
-import Grid, { Grid2 } from "./componetes/grid/grid";
+import Grid from "./componetes/grid/grid";
 
 function App() {
   return (
     <>
       <section>
         <Grid />
-      </section>
-      <section>
-        <Grid2 />
       </section>
 
     </>
